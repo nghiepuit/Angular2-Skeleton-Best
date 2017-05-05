bb9da98116ba35272670f3d18dc6a3d2bc9d116a
nghiepuit <nghiepuit@gmail.com>
2017-05-05 17:15:30 +0700

    t

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,37 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from 'ng2-translate';
-import {Subscription } from 'rxjs';
-import {ActivatedRoute} from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  	private subscription: Subscription;
+	private subscription: Subscription;
 
 	constructor(private translate: TranslateService, private activatedRoute: ActivatedRoute) {
-	    translate.addLangs(["en", "fr", "cn", "hi"]);
-	    translate.setDefaultLang('en');
-
-	    let browserLang = translate.getBrowserLang();
-	    translate.use(browserLang.match(/en|fr|cn|hi/) ? browserLang : 'en');
+		translate.addLangs(["en", "fr", "cn", "hi"]);
+		translate.setDefaultLang('en');
+		let browserLang = translate.getBrowserLang();
+		translate.use(browserLang.match(/en|fr|cn|hi/) ? browserLang : 'en');
 	}
 
-	changeLanguage(lang){
-	    this.translate.use(lang);
+	changeLanguage(lang) {
+		this.translate.use(lang);
 	}
 
 	ngOnInit() {
 		this.subscription = this.activatedRoute.queryParams.subscribe(
 			(param: any) => {
 				let locale = param['locale'];
-				if (locale !== undefined){
+				if (locale !== undefined) {
 					this.translate.use(locale);
 				}
-		});
+			});
 	}
 
 	ngOnDestroy() {
